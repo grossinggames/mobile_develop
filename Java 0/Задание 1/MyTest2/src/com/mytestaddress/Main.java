77d7d51c9cd2f88a91012cf4d0646807a9aa15d6
grpssinggames <grossinggames@gmail.com>
2016-02-28 20:50:41 +0300

    Добавлено задание 2

---
+++ Java 0/Задание 1/MyTest2/src/com/mytestaddress/Main.java
@@ -0,0 +1,13 @@
+package com.mytestaddress;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int [] arr = {22, 33, 44, 55};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Индекс " + (i + 1) + " значение: " + arr[i]);
+        }
+    }
+}
